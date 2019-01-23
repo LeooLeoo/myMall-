73527a44783426cfa5e8c5bbcec725e6496092a4
LeeoLeeo <leeoliu@163.com>
2019-01-23 21:34:10 +0100

    5-1

--- client/pages/trolley/trolley.js
+++ client/pages/trolley/trolley.js
@@ -1,4 +1,7 @@
 const app = getApp()
+const qcloud = require('../../vendor/wafer2-client-sdk/index')
+const config = require('../../config')
+
 
 Page({
 
@@ -6,7 +9,25 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    trolleyList: [{
+      id: 1,
+      name: '商品1',
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product1.jpg',
+      price: 45,
+      source: '海外·瑞典',
+      count: 1,
+    }, {
+      id: 2,
+      name: '商品2',
+      image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product2.jpg',
+      price: 158,
+      source: '海外·新西兰',
+      count: 3,
+    }], // 购物车商品列表
+    trolleyCheckMap: [undefined, true, undefined], // 购物车中选中的id哈希表
+    trolleyAccount: 45, // 购物车结算总价
+    isTrolleyEdit: false, // 购物车是否处于编辑状态
+    isTrolleyTotalCheck: true, // 购物车中商品是否全选
   },
 
   /**
@@ -14,6 +35,126 @@ Page({
    */
   onLoad: function (options) {
   
+  },
+
+  //单选按钮
+  onTapCheckSingle(event) {
+    let checkId = event.currentTarget.dataset.id
+    let trolleyCheckMap = this.data.trolleyCheckMap
+    let trolleyList = this.data.trolleyList
+    let isTrolleyTotalCheck = this.data.isTrolleyTotalCheck
+    let trolleyAccount = this.data.trolleyAccount
+    let numTotalProduct
+    let numCheckedProduct = 0
+
+    // 单项商品被选中/取消
+    trolleyCheckMap[checkId] = !trolleyCheckMap[checkId]
+
+    // 判断选中的商品个数是否需商品总数相等
+    numTotalProduct = trolleyList.length
+    trolleyCheckMap.forEach(checked => {
+      numCheckedProduct = checked ? numCheckedProduct + 1 : numCheckedProduct
+    })
+
+    isTrolleyTotalCheck = (numTotalProduct === numCheckedProduct) ? true : false
+
+    trolleyAccount = this.calcAccount(trolleyList, trolleyCheckMap)
+
+    this.setData({
+      trolleyCheckMap,
+      isTrolleyTotalCheck,
+      trolleyAccount
+    })
+  },
+
+  //全选按钮
+  onTapCheckTotal(event) {
+    let trolleyCheckMap = this.data.trolleyCheckMap
+    let trolleyList = this.data.trolleyList
+    let isTrolleyTotalCheck = this.data.isTrolleyTotalCheck
+    let trolleyAccount = this.data.trolleyAccount
+
+    // 全选按钮被选中/取消
+    isTrolleyTotalCheck = !isTrolleyTotalCheck
+
+    // 遍历并修改所有商品的状态
+    trolleyList.forEach(product => {
+      trolleyCheckMap[product.id] = isTrolleyTotalCheck
+    })
+
+    trolleyAccount = this.calcAccount(trolleyList, trolleyCheckMap)
+
+    this.setData({
+      isTrolleyTotalCheck,
+      trolleyCheckMap,
+      trolleyAccount
+    })
+
+  },
+
+  //编辑按钮
+  onTapEditTrolley() {
+    let isTrolleyEdit = this.data.isTrolleyEdit
+
+    if (isTrolleyEdit) {
+      this.updateTrolley()
+    } else {
+      this.setData({
+        isTrolleyEdit: !isTrolleyEdit
+      })
+    }
+  },
+
+
+  //更新购物车数据
+  updateTrolley() {
+    wx.showLoading({
+      title: '更新购物车数据...',
+    })
+
+    let trolleyList = this.data.trolleyList
+
+    qcloud.request({
+      url: config.service.updateTrolley,
+      method: 'POST',
+      login: true,
+      data: {
+        list: trolleyList
+      },
+      success: result => {
+        wx.hideLoading()
+
+        let data = result.data
+
+        if (!data.code) {
+          this.setData({
+            isTrolleyEdit: false
+          })
+        } else {
+          wx.showToast({
+            icon: 'none',
+            title: '更新购物车失败'
+          })
+        }
+      },
+      fail: () => {
+        wx.hideLoading()
+
+        wx.showToast({
+          icon: 'none',
+          title: '更新购物车失败'
+        })
+      }
+    })
+  },
+  //计算选中的商品总价
+  calcAccount(trolleyList, trolleyCheckMap) {
+    let account = 0
+    trolleyList.forEach(product => {
+      account = trolleyCheckMap[product.id] ? account + product.price * product.count : account
+    })
+
+    return account
   },
 
   /**
